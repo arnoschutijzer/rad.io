38b82ad72479e4c08dcb194c08fff27bce822393
Arno Schutijzer <arno.s@outlook.com>
2017-05-31 22:24:39 +0200

    fix(settings): remove backend settings and add example

---
+++ backend/src/config/settings.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'apiKey': 'YOUTUBE_API_KEY',
+  'secret': 'radiossecret',
+  'database': 'mongodb://127.0.0.1:27017/rad-io',
+  'port': 8080
+};
--- backend/src/config/settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'apiKey': 'GOOGLE_API_KEY',
-  'secret': 'JWT_SECRET',
-  'database': 'MONGODB_DB_ADDRESS',
-  'port': 9001
-};
