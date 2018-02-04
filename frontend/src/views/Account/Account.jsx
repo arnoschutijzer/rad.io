fa13b7304590eb51886620bba75fe619986f9155
Arno Schutijzer <arno.s@outlook.com>
2018-02-04 20:54:33 +0100

    fix(style): update settings page style

--- frontend/src/views/Account/Account.jsx
+++ frontend/src/views/Account/Account.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import './Account.scss';
+
 export default class Account extends Component {
   render() {
     return (
       <div className='view'>
         <div className='profile'>
-          <button onClick={ this.props.logout }>logout</button>
+          <div className="settings">
+            <button>do something else</button>
+            <button onClick={ this.props.logout }>logout</button>
+          </div>
         </div>
       </div>
     );
