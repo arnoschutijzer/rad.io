981dd4c4a163c95cab09928452404a5013475944
Arno Schutijzer <arno.s@outlook.com>
2018-02-09 00:32:56 +0100

    cleanup(backend): remove unused parameters from settings

--- backend/src/config/settings.example.js
+++ backend/src/config/settings.example.js
@@ -2,9 +2,5 @@ module.exports = {
   'API_KEY': 'YOUTUBE_API_KEY',
   'SECRET': 'radiossecret',
   'DATABASE': 'mongodb://127.0.0.1:27017/rad-io',
-  'PORT': 8080,
-  'URL': 'http://radio-backend.vleesbrood.xyz',
-  'FRONTEND_URL': 'http://radio.vleesbrood.xyz',
-  'SPOTIFY_CLIENT_ID': '<id>',
-  'SPOTIFY_CLIENT_SECRET': '<secret>'
+  'PORT': 8080
 };
