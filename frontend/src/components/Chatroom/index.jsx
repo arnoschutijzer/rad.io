4d4a56095b1cdc7c02786af04656840ca0495496
Arno Schutijzer <arno.s@outlook.com>
2018-02-09 19:18:28 +0100

    cleanup(design): remove dead code, make broadcast view a little more responsive
    relates to #45

--- frontend/src/components/Chatroom/index.jsx
+++ frontend/src/components/Chatroom/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './style.scss';
-import { Users } from '../Users';
 export default class Chatroom extends Component {
   constructor() {
     super();
