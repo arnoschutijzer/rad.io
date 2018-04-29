99e61fd6f7973ee923371594aec2b36aa6922347
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-04-29 23:56:25 +0200

    feat(config): use cli for development, run prod instead of dist

---
+++ frontend/starter.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const SRC = './src';
+
+module.exports = () => {
+  const webpack = {
+    entry: path.resolve(__dirname, SRC, 'index.jsx'),
+
+    resolve: {
+      alias: {
+        assets: path.join(__dirname, SRC, 'assets'),
+        components: path.join(__dirname, SRC, 'components'),
+        src: path.join(__dirname, SRC),
+        state: path.join(__dirname, SRC, 'state'),
+        views: path.join(__dirname, SRC, 'views')
+      }  
+    }
+  };
+
+  return {
+    webpack
+  };
+};
