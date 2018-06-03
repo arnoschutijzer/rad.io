94c7b6f0ca22b160e29780ddf731b867de6439fe
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-06-03 15:22:14 +0200

    feat: add initial test setup
    fixes #5

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'node': true
+    'node': true,
+    'jest/globals': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
@@ -13,7 +14,8 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
-    'react'
+    'react',
+    'jest'
   ],
   'rules': {
     'react/jsx-uses-react': 'error',
---
+++ frontend/src/components/RoomCard/RoomCard.test.js
@@ -0,0 +1,3 @@
+test('It is hello world!', () => {
+  expect('hello world!').toBe('hello world!');
+});
