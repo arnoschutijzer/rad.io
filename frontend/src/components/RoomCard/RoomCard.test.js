1090723a3a94f1098cd8467b2cc40be762b07647
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-06-05 20:48:55 +0200

    test(roomCard): roomCard should redirect to room

--- frontend/src/components/RoomCard/RoomCard.test.js
+++ frontend/src/components/RoomCard/RoomCard.test.js
@@ -16,21 +16,27 @@ test('<RoomCard /> mounts with no errors', () => {
 });
 
 test('<RoomCard /> renders a title', () => {
-  const wrapper = mount(<RoomCard room={roomMock}></RoomCard>);
+  const wrapper = mount(<RoomCard room={ roomMock }></RoomCard>);
   expect(wrapper.find('.room-title.title').exists()).toEqual(true);
   expect(wrapper.find('.room-title.title').text()).toEqual('this is a name');
 });
 
 test('<RoomCard /> renders a description', () => {
-  const wrapper = mount(<RoomCard room={roomMock}></RoomCard>);
+  const wrapper = mount(<RoomCard room={ roomMock }></RoomCard>);
   expect(wrapper.find('.description').exists()).toEqual(true);
   expect(wrapper.find('.description').text()).toEqual(roomMock.topic);
 });
 
 test('<RoomCard /> renders a creator', () => {
-  const wrapper = mount(<RoomCard room={roomMock}></RoomCard>);
+  const wrapper = mount(<RoomCard room={ roomMock }></RoomCard>);
   const creatorNode = (<p>
       created by { roomMock.creator.username }
   </p>);
   expect(wrapper.containsMatchingElement(creatorNode)).toEqual(true);
+});
+
+test('<RoomCard /> redirects to room', () => {
+  const wrapper = mount(<RoomCard room={ roomMock }></RoomCard>);
+  const roomCard = wrapper.find('.room').get(0);
+  expect(roomCard.props.to).toEqual(`/broadcast/${roomMock._id}`);
 });
