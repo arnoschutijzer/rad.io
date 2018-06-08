f08469bfabc581d96246d8ae819e7446594c03ab
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-06-08 18:41:09 +0200

    docs(config): use a relevant mongodb url in backend config example

--- backend/src/config/settings.example.js
+++ backend/src/config/settings.example.js
@@ -1,6 +1,6 @@
 module.exports = {
   'API_KEY': 'YOUTUBE_API_KEY',
   'SECRET': 'radiossecret',
-  'DATABASE': 'mongodb://127.0.0.1:27017/rad-io',
+  'DATABASE': 'mongodb://root:example@mongodb:27017',
   'PORT': 8080
 };
