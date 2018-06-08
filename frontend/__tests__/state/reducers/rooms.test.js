da1a65004ae416bae94edd4b647273d799a1e435
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-06-08 18:33:02 +0200

    test(rooms): test room reducer, remove dead code

--- frontend/__tests__/state/reducers/rooms.test.js
+++ frontend/__tests__/state/reducers/rooms.test.js
@@ -1,5 +1,31 @@
-import 'state/reducers/rooms';
+import {
+  FETCH_ROOMS_RESPONSE
+} from 'state/actions/rooms';
+import reduceRooms from 'state/reducers/rooms';
 
-test('placeholder', () => {
-  expect(true).toEqual(true);
+describe('reducers/rooms', () => {
+  test('should return initial state', () => {
+    const state = reduceRooms(undefined, {});
+    expect(state).toEqual({});
+  });
+
+  test('should map rooms by id', () => {
+    const firstRoom = {
+      _id: 'first',
+      name: 'first room'
+    };
+    const secondRoom = {
+      _id: 'second',
+      name: 'second room'
+    };
+    const rooms = [ firstRoom, secondRoom ];
+    const state = reduceRooms(undefined, {
+      type: FETCH_ROOMS_RESPONSE,
+      payload: rooms
+    });
+    expect(state).toEqual({
+      [ firstRoom._id ]: firstRoom,
+      [ secondRoom._id ]: secondRoom
+    });
+  });
 });
--- frontend/src/state/reducers/rooms.js
+++ frontend/src/state/reducers/rooms.js
@@ -1,5 +1,4 @@
 import {
-  FETCH_ROOM_RESPONSE,
   FETCH_ROOMS_RESPONSE
 } from '../actions/rooms';
 
@@ -8,13 +7,6 @@ const initialState = {};
 export default (state = initialState, action) => {
   if (action.type === FETCH_ROOMS_RESPONSE) {
     return mapRoomsToObject(action.payload);
-  }
-  if (action.type === FETCH_ROOM_RESPONSE) {
-    const mappedRoom = {
-      [action.payload._id]: action.payload
-    };
-
-    return Object.assign({}, state, mappedRoom);
   }
 
   return state;
