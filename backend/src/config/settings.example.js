9af789a4b2f884d4e126da9aceffe2928bcffdc0
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-06-08 19:19:33 +0200

    feat: use env variables, use node to start the application instead of nodemon so the application always restarts on error

--- backend/src/config/settings.example.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'API_KEY': 'YOUTUBE_API_KEY',
-  'SECRET': 'radiossecret',
-  'DATABASE': 'mongodb://root:example@mongodb:27017',
-  'PORT': 8080
-};
---
+++ backend/src/config/settings.js
@@ -0,0 +1,20 @@
+const getConfiguration = () => {
+  const RADIO_PORT = process.env.RADIO_PORT || 8080;
+  const RADIO_DATABASE = process.env.RADIO_DATABASE || 'mongodb://localhost:27017/rad-io';
+  const RADIO_SECRET = process.env.RADIO_SECRET || 'radiossecret';
+  const RADIO_API_KEY = process.env.RADIO_API_KEY || '';
+  
+  if (!RADIO_API_KEY) {
+    console.error('Could not start. No RADIO_API_KEY environment variable found.');
+    return process.exit(1);
+  }
+
+  return {
+    API_KEY: RADIO_API_KEY,
+    DATABASE: RADIO_DATABASE,
+    PORT: RADIO_PORT,
+    SECRET: RADIO_SECRET
+  };
+};
+
+module.exports = getConfiguration();
