df0a63c2d14086722351204bc41fa39a7530f84f
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-06-08 17:43:03 +0200

    test(auth): test auth reducer

--- frontend/__tests__/state/reducers/auth.test.js
+++ frontend/__tests__/state/reducers/auth.test.js
@@ -1,5 +1,113 @@
-import 'state/reducers/auth';
+import {
+  FETCH_PROFILE_RESPONSE,
+  LOGIN_REQUEST,
+  LOGIN_RESPONSE,
+  LOGOUT,
+  UNAUTHORIZED
+} from 'state/actions/auth';
+import reduceAuth from 'state/reducers/auth';
 
-test('placeholder', () => {
-  expect(true).toEqual(true);
+describe('reducers/auth', () => {
+  test('should return default state', () => {
+    const state = reduceAuth(undefined, {});
+    expect(state).toEqual({
+      loading: false
+    });
+  });
+
+  test('should return loading state', () => {
+    const state = reduceAuth(undefined, {
+      type: LOGIN_REQUEST
+    });
+
+    expect(state).toEqual({
+      loading: true
+    });
+  });
+
+  test('should add token & user to state', () => {
+    const payload = {
+      token: '1234-token',
+      user: {
+        username: 'test'
+      }
+    };
+    const state = reduceAuth(undefined, {
+      type: LOGIN_RESPONSE,
+      payload
+    });
+
+    expect(state).toEqual({
+      loading: false,
+      user: payload.user,
+      token: payload.token
+    });
+  });
+
+  test('should add token & user to state when defined in action', () => {
+    const payload = {
+      user: {
+        username: 'test'
+      }
+    };
+    const token = '1234-token';
+    const state = reduceAuth(undefined, {
+      type: FETCH_PROFILE_RESPONSE,
+      token,
+      payload
+    });
+
+    expect(state).toEqual({
+      loading: false,
+      user: payload.user,
+      token
+    });
+  });
+
+  test('should return initial state when unauthorized', () => {
+    // first create a state where we have an authenticated user
+    const payload = {
+      token: '1234-token',
+      user: {
+        username: 'test'
+      }
+    };
+    const authorizedState = reduceAuth(undefined, {
+      type: LOGIN_RESPONSE,
+      payload
+    });
+    expect(authorizedState).toEqual({
+      loading: false,
+      user: payload.user,
+      token: payload.token
+    });
+
+    // next fire the unauthorized action
+    const actualState = reduceAuth(authorizedState,{
+      type: UNAUTHORIZED
+    });
+    expect(actualState).toEqual({
+      loading: false
+    });
+  });
+
+  test('should return initial state when logout', () => {
+    const payload = {
+      token: '1234-token',
+      user: {
+        username: 'test'
+      }
+    };
+    const authorizedState = reduceAuth(undefined, {
+      type: LOGIN_RESPONSE,
+      payload
+    });
+
+    const loggedOutState = reduceAuth(authorizedState, {
+      type: LOGOUT
+    });
+    expect(loggedOutState).toEqual({
+      loading: false
+    });
+  });
 });
